549b4d1582f204231c62ef1d9efdea7d85391e03
Vitalii <vitalik.chu@mail.ru>
2025-07-31 16:09:44 +0300

    init

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ const CONFIG = {
 
 	// General
 	name: 'Vital',
-	imageBackground: false,
+	imageBackground: true,
 	openInNewTab: true,
 	twelveHourFormat: false,
 
@@ -31,7 +31,7 @@ const CONFIG = {
 	weatherUnit: 'C', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
 
-	trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
+	trackLocation: false, // If false or an error occurs, the app will use the lat/lon below
 	defaultLatitude: '57.6299',
 	defaultLongitude: '39.8737',
 
